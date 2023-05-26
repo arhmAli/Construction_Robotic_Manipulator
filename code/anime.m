4a595344871b93d25d304f908337d79bc3828afe
Arham Khan <arhamkhank353@gmail.com>
2023-05-26 12:13:33 +0500

    workspace,max_acc,other params found

--- code/anime.m
+++ code/anime.m
@@ -5,17 +5,11 @@ L3 = 5;
 L4 = 5;
 L5 = 5;
 L6 = 5;
-
-% DH parameters
 alpha = [0, pi/2, 0, 0, pi/2, 0];
 a = [0, 0, L2, L3, 0, 0];
 d = [L1, 0, 0, L4, L5, L6];
 theta = [0, 0, 0, 0, 0, 0];
-
-% Define the robot
 robot = SerialLink([theta', d', a', alpha'],'name','2019 MC 253');
-
-% Define the trajectory
 t = linspace(0, 5, 100);
 q1 = 0.5*sin(2*pi*t);
 q2 = 0.5*cos(2*pi*t);
@@ -24,25 +18,14 @@ q4 = 0.5*cos(4*pi*t);
 q5 = 0.5*sin(6*pi*t);
 q6 = 0.5*cos(6*pi*t);
 q = [q1', q2', q3', q4', q5', q6'];
-
-% Compute the maximum acceleration
 qd = diff(q)./diff(t)';
 qdd = diff(qd)./diff(t(1:end-1))';
 amax = max(sqrt(sum(qdd.^2, 2)));
-
-% Plot the trajectory
 figure;
 robot.plot(q,'trail','r');
 title('Trajectory of the robot');
 %name('2019-MC-253');
-
-% Display the maximum acceleration
 fprintf('Maximum acceleration: %f\n', amax);
-
-
-
-
-% Define robot and trajectory
 L1 = 1; L2 = 1; L3 = 1; L4 = 1; L5 = 1; L6 = 1;
 d1 = L1; d2 = 0; d3 = 0; d4 = L4; d5 = L5; d6 = L6;
 alpha = [0, pi/2, 0, 0, pi/2, 0];
